3f36d856d26f164129110fa5007b587ffc973ce0
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-06-07 22:58:42 +1000

    Fix updating course add receptions count

--- MedicineSchedule/MedicineSchedule/ViewModels/CourseViewModel.cs
+++ MedicineSchedule/MedicineSchedule/ViewModels/CourseViewModel.cs
@@ -1,18 +1,177 @@
 ﻿using System.ComponentModel;
 
+using Xamarin.Forms;
+
 using MedicineSchedule.Models;
 
 namespace MedicineSchedule.ViewModels
 {
 	public class CourseViewModel : INotifyPropertyChanged
 	{
+		public static Course StaticCourse;
+
 		public event PropertyChangedEventHandler PropertyChanged;
 
+		public Page ParentPage { get; set; }
+
+		public Command CreateCourseCommand { get; set; }
+		public Command UpdateCourseCommand { get; set; }
+		public Command DeleteCourseCommand { get; set; }
+		public Command CancelCommand { get; set; }
+
 		public Course Course { get; private set; }
+
+		public string Name
+		{
+			get => Course == null ? name : Course.MedicineName;
+			set
+			{
+				if (!string.IsNullOrEmpty(value)) {
+					if (Course == null) {
+						name = value;
+					} else {
+						Course.MedicineName = value;
+					}
+					OnPropertyChanged("Name");
+				}
+				CreateCourseCommand.ChangeCanExecute();
+				UpdateCourseCommand.ChangeCanExecute();
+			}
+		}
+
+		public int MedicineType
+		{
+			get => (int)(Course == null ? Course.MedicineType : medicineType);
+			set
+			{
+				if (Course == null) {
+					medicineType = (MedicineType)value;
+				} else {
+					Course.MedicineType = (MedicineType)value;
+				}
+				//OnPropertyChanged("MedicineType");
+			}
+		}
+
+		public int FoodRelation
+		{
+			get => (int)(Course == null ? Course.FoodRelation : foodRelation);
+			set
+			{
+				if (Course == null) {
+					foodRelation = (FoodRelation)value;
+				} else {
+					Course.FoodRelation = (FoodRelation)value;
+				}
+				//OnPropertyChanged("FoodRelation");
+			}
+		}
+
+		public string ReceptionsInDay
+		{
+			get => (Course == null ? Course.ReceptionInDayCount : receptionsInDayCount).ToString();
+			set
+			{
+				if (int.TryParse(value, out int temp)) {
+					if (Course == null) {
+						receptionsInDayCount = temp;
+					} else {
+						Course.ReceptionInDayCount = temp;
+					}
+				} else {
+					if (Course == null) {
+						receptionsInDayCount = 0;
+					} else {
+						Course.ReceptionInDayCount = 0;
+					}
+				}
+				CreateCourseCommand.ChangeCanExecute();
+				UpdateCourseCommand.ChangeCanExecute();
+				//OnPropertyChanged("FoodRelation");
+			}
+		}
+
+		private string name;
+		private MedicineType medicineType;
+		private FoodRelation foodRelation;
+		private int receptionsInDayCount;
 
 		public CourseViewModel(Course course = null)
 		{
 			Course = course;
+			if (course == null && StaticCourse != null) {
+				Course = StaticCourse;
+			}
+			if (Course != null) {
+				name = Course.MedicineName;
+				medicineType = Course.MedicineType;
+				foodRelation = Course.FoodRelation;
+				receptionsInDayCount = Course.ReceptionInDayCount;
+			}
+			
+			CreateCourseCommand = new Command(CreateCourse, Validate);
+			UpdateCourseCommand = new Command(UpdateCourse, Validate);
+			DeleteCourseCommand = new Command(DeleteCourse);
+			CancelCommand = new Command(Cancel);
+		}
+
+		protected void OnPropertyChanged(string propName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+		}
+
+		private async void CreateCourse()
+		{
+			if (Course == null) {
+				StaticCourse = new Course() {
+					MedicineName = name,
+					MedicineType = medicineType,
+					FoodRelation = foodRelation,
+					ReceptionInDayCount = receptionsInDayCount
+				};
+			}
+			await ParentPage.Navigation.PopModalAsync();
+		}
+
+		private async void UpdateCourse()
+		{
+			if (Course != null) {
+				StaticCourse = Course;
+			}
+			await ParentPage.Navigation.PopModalAsync();
+		}
+
+		private async void DeleteCourse()
+		{
+			if (Course != null) {
+				bool userIsSure = await ParentPage.DisplayAlert("Вы уверены?", "Вернуть курс будет нельзя", "Да", "Нет");
+				if (userIsSure) {
+					Course = null;
+					StaticCourse = null;
+					await ParentPage.Navigation.PopModalAsync();
+				}
+			}
+		}
+
+		private async void Cancel()
+		{
+			bool userIsSure = await ParentPage.DisplayAlert("Вы уверены?", "Изменения не будут сохранены", "Да", "Нет");
+			if (userIsSure) {
+				await ParentPage.Navigation.PopModalAsync();
+			}
+		}
+
+		private bool Validate()
+		{
+			if (Course == null) {
+				return
+					!string.IsNullOrEmpty(name) &&
+					(receptionsInDayCount >= 1 && receptionsInDayCount <= 12);
+			} else {
+				return
+					!string.IsNullOrEmpty(Course.MedicineName) &&
+					(Course.ReceptionInDayCount >= 1 && Course.ReceptionInDayCount <= 12);
+			}
 		}
 	}
 }
--- MedicineSchedule/MedicineSchedule/Views/CourseView.xaml.cs
+++ MedicineSchedule/MedicineSchedule/Views/CourseView.xaml.cs
@@ -2,20 +2,48 @@
 using Xamarin.Forms.Xaml;
 
 using MedicineSchedule.ViewModels;
+using System.ComponentModel;
+using System;
 
 namespace MedicineSchedule.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CourseView : ContentPage
 	{
-		public CourseViewModel Course { get; private set; }
+		public CourseViewModel ViewModel { get; private set; }
 
-		public CourseView(CourseViewModel course)
+		public CourseView(CourseViewModel viewModel)
 		{
-			InitializeComponent();
+			ViewModel = viewModel;
 
-			Course = course;
-			BindingContext = Course;
+			InitializeComponent();
+			
+			ViewModel.ParentPage = this;
+			BindingContext = ViewModel;
+			if (ViewModel.Course == null) {
+				DeleteBtn.IsVisible = false;
+				SaveBtn.IsVisible = false;
+			} else {
+				CreateBtn.IsVisible = false;
+			}
+		}
+
+		private void CheckCountEntry(object sender, EventArgs eventArgs)
+		{
+			if (string.IsNullOrEmpty(ReceptionsInDayEntry.Text)) {
+				ReceptionsInDayErrorMsg.Text = "Вы забыли ввести число";
+				ReceptionsInDayErrorMsg.IsVisible = true;
+			} else if (!int.TryParse(ReceptionsInDayEntry.Text, out int count)) {
+				ReceptionsInDayErrorMsg.Text = "Введите целое число";
+				ReceptionsInDayErrorMsg.IsVisible = true;
+			} else if (count < 1 || count > 12) {
+				ReceptionsInDayErrorMsg.Text = "Введите число от 1 до 12";
+				ReceptionsInDayErrorMsg.IsVisible = true;
+			} else {
+				ReceptionsInDayErrorMsg.Text = "";
+				ReceptionsInDayErrorMsg.IsVisible = false;
+				ViewModel.ReceptionsInDay = count.ToString();
+			}
 		}
 	}
 }
