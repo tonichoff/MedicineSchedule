733ca5144619490ad485cd4a601a3eae05752e96
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-06-16 01:38:31 +1000

    Fix double showing notification page

--- MedicineSchedule/MedicineSchedule/App.xaml.cs
+++ MedicineSchedule/MedicineSchedule/App.xaml.cs
@@ -8,12 +8,16 @@ namespace MedicineSchedule
 {
 	public partial class App : Application
 	{
+		private bool IsSleep;
+
 		public App()
 		{
 			InitializeComponent();
 
 			NotificationCenter.Current.NotificationReceived += (eventArgs) => {
-				ShowNotificationPage(eventArgs.Request.NotificationId);
+				if (!IsSleep) {
+					ShowNotificationPage(eventArgs.Request.NotificationId);
+				}
 			};
 			NotificationCenter.Current.NotificationTapped += (eventArgs) => {
 				ShowNotificationPage(eventArgs.Request.NotificationId);
@@ -27,6 +31,18 @@ namespace MedicineSchedule
 					MainPage.Navigation.PushModalAsync(new NotificationPage(notificationId));
 				});
 			}
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+			IsSleep = true;
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			IsSleep = false;
 		}
 	}
 }
